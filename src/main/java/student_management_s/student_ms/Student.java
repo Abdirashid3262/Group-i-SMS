6cff2319c9172bba0e0dc7f783ece825dde1c512
ahmed <ahmedabdirashidali3262@gmail.com>
2024-07-24 19:33:07 +0300

    This is Student Management System

---
+++ src/main/java/student_management_s/student_ms/Student.java
@@ -0,0 +1,61 @@
+package student_management_s.student_ms;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+//Modle Service that include all service (Classes)
+//studentID , studentName, faculty, tell
+public class Student {
+    public Long getId() {
+        return id;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public String getTell() {
+        return tell;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public void setTell(String tell) {
+        this.tell = tell;
+    }
+
+    private String studentID;
+    private String studentName;
+    private String faculty;
+    private String tell;
+}
